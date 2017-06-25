66a74e855b017275f1da9dbcbac17d1e0beecef1
ght <597315261@qq.com>
2017-06-26 00:54:46 +0800

    fixbug
    修复BUG 当apk文件不在该工具路径下时，无法读取签名配置参数的问题

--- lib/readconfig.bat
+++ lib/readconfig.bat
@@ -1,3 +1,7 @@
 @echo off
-@for /f "skip=1 tokens=1,2 delims==" %%a IN (./config.ini) Do if %1==%%a set %2=%%b & @echo readconfig get %%a, value is %%b
+pushd "%cd%"
+cd ..
+set pard=%cd%
+popd
+@for /f "skip=1 tokens=1,2 delims==" %%a IN (%pard%/config.ini) Do if %1==%%a set %2=%%b & @echo readconfig get %%a, value is %%b
 @goto :eof
